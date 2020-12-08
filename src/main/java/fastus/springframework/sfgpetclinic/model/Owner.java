3f7b7ea19fbfe7fe5cfdb9ca7ce503a4987917f8
Fastus001 <tkarczmarzyk@gmail.com>
2020-12-08 21:28:38 +0100

    Adding POJOs Closes #2

---
+++ src/main/java/fastus/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package fastus.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/fastus/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package fastus.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
