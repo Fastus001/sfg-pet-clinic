e01175c6d9d766215d5dff314678800238198e80
Fastus001 <tkarczmarzyk@gmail.com>
2020-12-12 21:38:21 +0100

    cleaning

--- pet-clinic-web/src/main/java/fastus/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/fastus/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package fastus.springframework;
+package fastus.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
