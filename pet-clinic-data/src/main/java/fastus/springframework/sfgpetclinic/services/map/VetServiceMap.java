291d4828c207c583ff5a679a593d6bd8e610d47a
Fastus001 <tkarczmarzyk@gmail.com>
2020-12-16 22:25:37 +0100

    Impl Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package fastus.springframework.sfgpetclinic.services.map;
+
+import fastus.springframework.sfgpetclinic.model.Vet;
+import fastus.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
