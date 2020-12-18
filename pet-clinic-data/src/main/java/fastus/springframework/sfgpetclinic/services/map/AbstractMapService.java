1711ae23edaac76ec0a965655f1cd5d8c8655c93
Fastus001 <tkarczmarzyk@gmail.com>
2020-12-18 15:34:51 +0100

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package fastus.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import fastus.springframework.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
-    protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
+    protected Map<Long,T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -16,9 +15,16 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id,object);
+    T save(T object){
 
+        if(object != null){
+            if (object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }else {
+            throw  new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -28,5 +34,16 @@ public abstract class AbstractMapService<T,ID> {
 
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId(){
+        Long nextID;
+
+        try{
+            nextID = Collections.max(map.keySet())+1;
+        }catch (NoSuchElementException e){
+            nextID = 1L;
+        }
+        return nextID;
     }
 }
