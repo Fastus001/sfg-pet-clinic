e1a751f92a3a3b66c4c8307728a7de8c69e5f42b
Fastus001 <tkarczmarzyk@gmail.com>
2020-12-28 14:17:28 +0100

    Added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package fastus.springframework.sfgpetclinic.services;
+
+import fastus.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
