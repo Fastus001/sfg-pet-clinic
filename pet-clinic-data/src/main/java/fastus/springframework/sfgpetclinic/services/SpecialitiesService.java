915ac32b27ac3c4accfd9f10255a35833a6b0efc
Fastus001 <tkarczmarzyk@gmail.com>
2020-12-28 16:58:34 +0100

    Addes Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,10 @@
+package fastus.springframework.sfgpetclinic.services;
+
+import fastus.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Tom - 28.12.2020
+ */
+public interface SpecialitiesService extends CrudService<Speciality,Long>{
+
+}
