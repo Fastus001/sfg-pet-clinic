81b76817207416eb85e20c5b36a94e90b2a11004
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-17 22:15:35 +0100

    Updated Vet to use JPA. Closes #37

--- pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/model/Speciality.java
@@ -1,10 +1,19 @@
 package fastus.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Tom - 28.12.2020
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
+
+    @Column(name = "description")
     private String description;
+
 
     public String getDescription() {
         return description;
