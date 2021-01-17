99b64ab1be504cdec727c538f34692540e8252c1
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-17 22:21:59 +0100

    Create Visit JPA, Closes #38

--- pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/model/Visit.java
@@ -1,16 +1,24 @@
 package fastus.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by Tom - 28.12.2020
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
-
-
 
     public LocalDate getDate() {
         return date;
