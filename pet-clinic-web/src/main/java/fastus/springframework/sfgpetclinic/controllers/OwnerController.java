a38563a514808fc478bd853ba561bff9e4639ebd
Fastus001 <tkarczmarzyk@gmail.com>
2021-02-03 22:42:09 +0100

    SPC - Display Owner Record

--- pet-clinic-web/src/main/java/fastus/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/fastus/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package fastus.springframework.sfgpetclinic.controllers;
 import fastus.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -28,5 +31,12 @@ public class OwnerController {
     public String findOwners(){
 
         return "notImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
