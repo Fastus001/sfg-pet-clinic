3dd74da5f5bb823bda2a404add9c5e26f50bb1f7
Fastus001 <tkarczmarzyk@gmail.com>
2021-02-07 21:54:17 +0100

    added add pet, update pet. #56

--- pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/fastus/springframework/sfgpetclinic/model/Owner.java
@@ -14,13 +14,13 @@ import java.util.Set;
 @NoArgsConstructor
 @Entity
 @Table(name = "owners")
-public class Owner extends Person{
+public class Owner extends Person {
 
     @Column(name = "address")
     private String address;
 
     @Column(name = "city")
-    private String  city;
+    private String city;
 
     @Column(name = "telephone")
     private String telephone;
@@ -34,6 +34,39 @@ public class Owner extends Person{
         this.address = address;
         this.city = city;
         this.telephone = telephone;
-        this.pets = pets;
+
+        if (pets != null) {
+            this.pets = pets;
+        }
+    }
+
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
     }
 }
