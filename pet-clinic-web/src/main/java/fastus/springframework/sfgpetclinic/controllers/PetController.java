656bf79af2c6da7d0aa39bf3446eba48c1c10d9d
Fastus001 <tkarczmarzyk@gmail.com>
2021-02-07 21:54:17 +0100

    added add pet, update pet. close #56

--- pet-clinic-web/src/main/java/fastus/springframework/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/fastus/springframework/sfgpetclinic/controllers/PetController.java
@@ -1,17 +1,19 @@
 package fastus.springframework.sfgpetclinic.controllers;
 
 import fastus.springframework.sfgpetclinic.model.Owner;
+import fastus.springframework.sfgpetclinic.model.Pet;
 import fastus.springframework.sfgpetclinic.model.PetType;
 import fastus.springframework.sfgpetclinic.services.OwnerService;
 import fastus.springframework.sfgpetclinic.services.PetService;
 import fastus.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 /**
@@ -20,7 +22,7 @@ import java.util.Collection;
 @Controller
 @RequestMapping("/owners/{ownerId}")
 public class PetController {
-    private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "owners/createOrUpdateOwnerForm";
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
     private final PetService petService;
     private final PetTypeService petTypeService;
     private final OwnerService ownerService;
@@ -44,6 +46,49 @@ public class PetController {
     @InitBinder("owner")
     public  void initOwnerBinder(WebDataBinder dataBinder){
         dataBinder.setDisallowedFields("id");
+    }
+
+    @GetMapping("/pets/new")
+    public String initCreationForm(Owner owner, Model model){
+        Pet pet = new Pet();
+        owner.getPets().add(pet);
+        model.addAttribute("pet",pet);
+        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/pets/new")
+    public String processCreationForm(Owner owner, @Valid Pet pet, BindingResult result, Model model){
+        if(StringUtils.hasLength(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(),true) !=null){
+            result.rejectValue("name", "duplicate", "already exists");
+        }
+        owner.getPets().add(pet);
+        if(result.hasErrors()){
+            model.addAttribute("pet",pet);
+            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        }else {
+            petService.save(pet);
+
+            return "redirect:/owners/" + owner.getId();
+        }
+    }
+
+    @GetMapping("/pets/{petId}/edit")
+    public String initUpdateForm(@PathVariable Long petId, Model model){
+        model.addAttribute("pet", petService.findById(petId));
+        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/pets/{petId}/edit")
+    public String processUpdateForm(@Valid Pet pet, BindingResult result, Owner owner, Model model){
+        if(result.hasErrors()){
+            pet.setOwner(owner);
+            model.addAttribute("pet", pet);
+            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        }else{
+            owner.getPets().add(pet);
+            petService.save(pet);
+            return "redirect:/owners/" + owner.getId();
+        }
     }
 
 }
--- pet-clinic-web/src/test/java/fastus/springframework/sfgpetclinic/controllers/PetControllerTest.java
+++ pet-clinic-web/src/test/java/fastus/springframework/sfgpetclinic/controllers/PetControllerTest.java
@@ -1,6 +1,7 @@
 package fastus.springframework.sfgpetclinic.controllers;
 
 import fastus.springframework.sfgpetclinic.model.Owner;
+import fastus.springframework.sfgpetclinic.model.Pet;
 import fastus.springframework.sfgpetclinic.model.PetType;
 import fastus.springframework.sfgpetclinic.services.OwnerService;
 import fastus.springframework.sfgpetclinic.services.PetService;
@@ -17,9 +18,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -60,12 +64,48 @@ class PetControllerTest {
         when(ownerService.findById(anyLong())).thenReturn(owner);
         when(petTypeService.findAll()).thenReturn(petTypes);
 
-        mockMvc.perform(get("owners/1/pet/new"))
+        mockMvc.perform(get("/owners/1/pets/new"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("owner"))
                 .andExpect(model().attributeExists("pet"))
                 .andExpect(view().name("pets/createOrUpdatePetForm"));
     }
+
+    @Test
+    void processCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(post("/owners/1/pets/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(petService).save(any());
+    }
+
+    @Test
+    void initUpdateForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+        when(petService.findById(anyLong())).thenReturn(Pet.builder().id(2L).build());
+
+        mockMvc.perform(get("/owners/1/pets/2/edit"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    void processUpdateForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(post("/owners/1/pets/2/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+    }
+
 
     @Test
     void populatePetTypes() {
